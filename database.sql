ebc7fccd60bc3d811db9c702e72363260085386f
Akbar <116638921+Akbar-07@users.noreply.github.com>
2023-08-10 17:46:32 +0500

    aaaa
    aaa

---
+++ database.sql
@@ -0,0 +1,92 @@
+create table aksiya(
+    "id" serial primary key,
+    "title" VARCHAR (50) NOT NULL,
+    "image" VARCHAR (50),
+    "description" VARCHAR (50),
+    "start_day" date,
+    "end_day" date,
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+
+)
+create table news(
+    "id" serial primary key,
+    "title" VARCHAR (50),
+    "description" VARCHAR (50),
+    "image" VARCHAR (50),
+    "min_description" text,
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+create table compony(
+    "id" serial primary key,
+    "logo"text,
+    "phone" VARCHAR (50)
+    "telegram" VARCHAR (50)
+    "email" VARCHAR (50) NOT NULL,
+    "whatsapp" VARCHAR (50),
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+create table homeiy(
+    "id" serial primary key,
+    "image" VARCHAR (50),
+    "link" VARCHAR (50) NOT NULL,
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+
+)
+create table skachat_pridlachenu(
+    "id" serial primary key,
+    "image" VARCHAR (50),
+    "title" VARCHAR (50) ,
+    "deskription" VARCHAR (50),
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+create table preferences (
+    "id" serial primary key,
+    "image" VARCHAR (50),
+    "title" VARCHAR (50) ,
+    "description" VARCHAR (50),
+    "liso" VARCHAR CHECK("F" || "Y"),
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+create table category(
+    "id" serial primary key,
+    "image" VARCHAR (50),
+    "title" VARCHAR (50) ,
+    "description" VARCHAR (50),
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+create table product(
+    "id" serial primary key,
+    "description" VARCHAR (50),
+    "sena" integer default NOT NULL,
+    "marka" text,
+    "stay" text,
+    "liquid" text,
+    "storage" text,
+    "against"text,
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+create table tarif(
+    "id" serial primary key,
+    "image" VARCHAR (50),
+    "title" VARCHAR (50),
+    "map_meter" integer default NOT NULL,
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
+
+create table starter(
+    "id" serial primary key,
+    "mini_asphalt" text,
+    "car_width" integer default NOT NULL,
+    "car_heigth" integer default NOT NULL,
+    "time_create" timestamp default current_timestamp NOT NULL,
+    "time_update" timestamp default current_timestamp NOT NULL,
+)
