1e6883918b989c503667867c46b93444211946c7
Akbar <86982819+abbassiriyus@users.noreply.github.com>
2023-08-12 14:40:23 +0500

    aaa
    aaa

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-create table action(
+create table aksiya(
     "id" serial primary key,
     "title" VARCHAR (50) NOT NULL,
     "image" VARCHAR (50),
@@ -7,10 +7,11 @@ create table action(
     "end_day" date,
     "time_create" timestamp default current_timestamp not null,
     "time_update" timestamp default current_timestamp not null
+
 )
 create table news(
     "id" serial primary key,
-    "title" VARCHAR (50) NOT NULL,
+    "title" VARCHAR (50),
     "description" VARCHAR (50),
     "image" VARCHAR (50),
     "min_description" text,
@@ -33,6 +34,7 @@ create table homeiy(
     "link" VARCHAR (50) NOT NULL,
     "time_create" timestamp default current_timestamp not null,
     "time_update" timestamp default current_timestamp not null
+
 )
 create table skachat_pridlachenu(
     "id" serial primary key,
@@ -47,9 +49,10 @@ create table preferences (
     "image" VARCHAR (50),
     "title" VARCHAR (50) ,
     "description" VARCHAR (50),
-    "liso" VARCHAR  (1),
+    "liso" VARCHAR CHECK ("F"||"Y"),
     "time_create" timestamp default current_timestamp not null,
     "time_update" timestamp default current_timestamp not null
+    
 )
 create table category(
     "id" serial primary key,
@@ -69,7 +72,7 @@ create table product(
     "storage" text,
     "against"text,
     "time_create" timestamp default current_timestamp not null,
-    "t;ime_update" timestamp default current_timestamp not null
+    "time_update" timestamp default current_timestamp not null
 )
 create table tarif(
     "id" serial primary key,
@@ -79,6 +82,7 @@ create table tarif(
     "time_create" timestamp default current_timestamp not null,
     "time_update" timestamp default current_timestamp not null
 )
+
 create table starter(
     "id" serial primary key,
     "mini_asphalt" text,
