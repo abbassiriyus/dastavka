ddef338ec05f982956dbf4ca8a9a1ec3798dec8e
Akbar-07 <86982819+abbassiriyus@users.noreply.github.com>
2023-08-15 14:42:29 +0300

    asd
    sa

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 
 const express = require("express")
 const app = express()
-const cors = require("cors")
-const fileUpload = require("express-fileupload")
-const bodyParser = require('body-parser');
+// const cors = require("cors")
+// const fileUpload = require("express-fileupload")
+// const bodyParser = require('body-parser');
 
 
-// import
-const aksiya=require('./routes/aksiya.js')
+// // import
+// const aksiya=require('./routes/aksiya.js')
 
 
 
 
 
-const pool = require("./db")
+// const pool = require("./db")
 
 
-app.use(fileUpload())
-app.use(cors())
-app.use(express.static('./media'))
+// app.use(fileUpload())
+// app.use(cors())
+// app.use(express.static('./media'))
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use('/api',aksiya)
+// app.use('/api',aksiya)
 
 
 
